d33869e3f44e01e26abc2889e9c126a86727dc71
Zhenghua (Calvin) Chen <41121094+calchen01@users.noreply.github.com>
2018-11-26 17:38:08 -0500

    Update UnpluggAPISecretFile.java

--- UnpluggAPISecretFile.java
+++ UnpluggAPISecretFile.java
@@ -1,7 +1,5 @@
 public class UnpluggAPISecretFile {
-
 	final static String endPoint = "https://maps.googleapis.com";
 	final static String staticMapsPath = "/maps/api/staticmap?";
-	final static String key = "01a79e98280502293f876adc3a922a87480c21ffcccf0d73aac20102629432b0";
-	
+	final static String key = "";
 }
