0f67999ff6a285aa9c16ef269bfa4f570a1cb77c
Jill Liu <shiqingliu1108@gmail.com>
2018-11-27 13:37:26 -0500

    deleted duplicate files

--- UnpluggAPISecretFile.java
+++
@@ -1,5 +0,0 @@
-public class UnpluggAPISecretFile {
-	final static String endPoint = "https://maps.googleapis.com";
-	final static String staticMapsPath = "/maps/api/staticmap?";
-	final static String key = "";
-}
